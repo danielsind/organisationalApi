e4765bff1e99abf784664cce508c500a5ecd44a3
danielsind <danielsindet@gmail.com>
2020-02-19 10:26:27 +0300

    Added setUp() and tearDown() in DepartmentTest.java

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -10,6 +10,14 @@ public class DepartmentTest {
     public Department setupNewDepartment(){
         return new Department("accounting", "handles company's finance", 5);
     }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
     @Test
     public void departmentInstantiatesCorrectly_true() {
         Department newDepartment = setupNewDepartment();
