100a5c4853449c670b648d4f8142a716a3f92206
danielsind <danielsindet@gmail.com>
2020-02-19 10:24:54 +0300

    Tets for Department INstantiantion if it it is correct

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,4 +1,19 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class DepartmentTest {
+    public Department setupNewDepartment(){
+        return new Department("accounting", "handles company's finance", 5);
+    }
+    @Test
+    public void departmentInstantiatesCorrectly_true() {
+        Department newDepartment = setupNewDepartment();
+        assertEquals(true, newDepartment instanceof Department);
+    }
 }
+
